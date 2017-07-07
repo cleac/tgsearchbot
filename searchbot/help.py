ae424895e54776532fb4e330561010550d2d3b51
Alexander Nesterenko <alexcleac@gmail.com>
2017-07-07 19:59:10 +0300

    Edit help message

--- searchbot/help.py
+++ searchbot/help.py
@@ -4,5 +4,4 @@ Hello!
 
 I am a telegram bot that generates duckduckgo links from tg directly.
 I am an inline bot, you can access it via @{bot_name}.
-Also I can reply you with link if you write me directly!
     '''.format(bot_name)
