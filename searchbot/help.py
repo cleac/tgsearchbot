8ce6483f01648c93bad563f13f73a247362d35c1
Alexander Nesterenko <alexcleac@gmail.com>
2017-07-07 20:03:22 +0300

    Improve help message

--- searchbot/help.py
+++ searchbot/help.py
@@ -4,4 +4,6 @@ Hello!
 
 I am a telegram bot that generates duckduckgo links from tg directly.
 I am an inline bot, you can access it via @{bot_name}.
+
+If you have any questions feel free to leave issue at http://github.com/cleac/tgsearchbot
     '''.format(bot_name)
